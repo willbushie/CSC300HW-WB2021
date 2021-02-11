a5edecd63804fd817b88c3c8200bc950cfaa5edd
Mike Litman <awesomefat@gmail.com>
2021-02-11 11:19:39 -0600

    changed stuff

--- driver.cpp
+++ driver.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv)
 {
-    Card* KingKrush = new Card("King Krush", 9, 8, 8);
+    Card* KingKrush = new Card("King Krush", 8, 8, 8);
     //(*KingKrush).display();
     KingKrush->display();
 }
