efaedc3712d653162db6a4a1a28f227e9adba1c9
Mike Litman <awesomefat@gmail.com>
2021-02-16 10:15:29 -0600

    changed stuff

---
+++ curlTest.cpp
@@ -0,0 +1,64 @@
+#include <string>
+#include <iostream>
+#include <curl/curl.h>
+
+using namespace std;
+
+//
+// For this demonstration, the contents of the web page are put
+// into this global variable.
+//
+string contents;
+
+//
+// This is the callback function that is called by
+// curl_easy_perform(curl)
+//
+size_t handle_data(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+    int numbytes = size*nmemb;
+    // The data is not null-terminated, so get the last character, and replace
+    // it with '\0'.
+    char lastchar = *((char *) ptr + numbytes - 1);
+    *((char *) ptr + numbytes - 1) = '\0';
+    contents.append((char *)ptr);
+    contents.append(1,lastchar);
+    *((char *) ptr + numbytes - 1) = lastchar;  // Might not be necessary.
+    return size*nmemb;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        cerr << "use: curltest url\n";
+        return -1;
+    }
+
+    CURL* curl = curl_easy_init();
+    if(curl)
+    {
+        // Tell libcurl the URL
+        curl_easy_setopt(curl,CURLOPT_URL, argv[1]);
+        // Tell libcurl what function to call when it has data
+        curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,handle_data);
+        // Do it!
+        CURLcode res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        if (res == 0)
+        {
+            cout << contents << endl;
+            string temp = "";
+            for(int i = 0; i < 5; i++)
+            {
+                temp += contents[i];
+            }
+            cout << temp << endl;
+        }
+        else
+        {
+            cerr << "Error: " << res << endl;
+        }
+    }
+    return 0;
+}
--- driver.cpp
+++ driver.cpp
@@ -1,8 +1,26 @@
 #include "Card.hpp"
+#include <iostream>
+#include <string.h>
+
+using namespace std;
 
 int main(int argc, char** argv)
 {
-    Card* KingKrush = new Card("King Krush", 8, 8, 8);
+    //Card* KingKrush = new Card("King Krush", 8, 8, 8);
     //(*KingKrush).display();
-    KingKrush->display();
+    //KingKrush->display();
+    char url[1000] = "https://api.hearthstonejson.com/v1/25770/enUS/cards.json";
+    
+    fstream fs;
+    fs.open(url);
+    string str;
+    getline(in,str);  // Get the frist line from the file, if any.
+    while ( in ) 
+    {  // Continue if the line was sucessfully read.
+        cout << str << endl;  // Process the line.
+        getline(in,str);   // Try to get another line.
+    }
+    fs.close();
+
+    return 0;
 }
