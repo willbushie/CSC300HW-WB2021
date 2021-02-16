a09e9d803d842138def1f2fbe7bb8077c4ee3971
Mike Litman <awesomefat@gmail.com>
2021-02-16 11:24:45 -0600

    changed stuff

--- driver.cpp
+++ driver.cpp
@@ -11,16 +11,7 @@ int main(int argc, char** argv)
     //KingKrush->display();
     char url[1000] = "https://api.hearthstonejson.com/v1/25770/enUS/cards.json";
     
-    fstream fs;
-    fs.open(url);
-    string str;
-    getline(in,str);  // Get the frist line from the file, if any.
-    while ( in ) 
-    {  // Continue if the line was sucessfully read.
-        cout << str << endl;  // Process the line.
-        getline(in,str);   // Try to get another line.
-    }
-    fs.close();
+    
 
     return 0;
 }
