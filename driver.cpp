456bf654c32d71c50c8c58e6433c730519a63a4e
Mike Litman <awesomefat@gmail.com>
2021-02-18 11:33:42 -0600

    changed stuff

--- driver.cpp
+++ driver.cpp
@@ -7,7 +7,9 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    URL* u1 = new URL("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
-    cout << u1->getContents() << endl;
+    //URL* u1 = new URL("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
+    //cout << u1->getContents() << endl;
+    string jsonString = "[{\"fname\":\"val1\", \"lname\": \"val2\", \"sums\":[1,2,3]},{\"fname\":\"val3\", \"lname\":\"val4\"}]";
+    cout << jsonString << endl;
     return 0;
 }
