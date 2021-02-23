28ed142739bf2f24dd8293d25bae53debb3ae3c8
Mike Litman <awesomefat@gmail.com>
2021-02-23 10:45:50 -0600

    changed stuff

--- driver.cpp
+++ driver.cpp
@@ -1,7 +1,8 @@
 #include "Card.hpp"
 #include "URL.hpp"
 #include <iostream>
-#include <string.h>
+#include <string>
+#include "myFunctions.hpp"
 
 using namespace std;
 
@@ -11,5 +12,38 @@ int main(int argc, char** argv)
     //cout << u1->getContents() << endl;
     string jsonString = "[{\"fname\":\"val1\", \"lname\": \"val2\", \"sums\":[1,2,3]},{\"fname\":\"val3\", \"lname\":\"val4\"}]";
     cout << jsonString << endl;
+    string answer = processJSON(jsonString);
+    cout << answer << endl;
+    
     return 0;
+}
+
+string processJSON(string json)
+{
+    string temp = "";
+    if(json[0] == '[')
+    {
+        int countSquareBrackets = 1;
+        //process contents of the array
+        for(int i = 1; i < json.length(); i++)
+        {
+            if(json[i] == '[')
+            {
+                countSquareBrackets++;
+            }
+            else if(json[i] == ']')
+            {
+                countSquareBrackets--;
+                if(countSquareBrackets == 0)
+                {
+                    return temp;
+                }
+            }
+            temp = temp + json[i];
+            
+        }
+
+    }
+    return "Something went wrong!!!";
+
 }
---
+++ myFunctions.hpp
@@ -0,0 +1,5 @@
+#include <string>
+
+using namespace std;
+
+string processJSON(string json);
