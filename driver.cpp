8e315cdf14325f033ba48f705b8ec8685847f01f
Mike Litman <awesomefat@gmail.com>
2021-03-04 11:15:57 -0600

    changed stuff

--- driver.cpp
+++ driver.cpp
@@ -4,16 +4,39 @@
 #include <string>
 #include "myFunctions.hpp"
 #include "LinkedList.hpp"
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
+#define ARRAY_LENGTH 10 //called a macro
+ 
+//prototype functions
+void insertionSort(int ar[]);
+void displayIntArray(int ar[]);
+
 int main(int argc, char** argv)
 {
+    srandom(time(NULL));
+    long val; 
+    int ar[ARRAY_LENGTH];
+
+    for(int i = 0; i < ARRAY_LENGTH; i++)
+    {
+        val = random();
+        val = (val % 1000)+1;
+        ar[i] = val;
+    }
+
+    //displayIntArray(ar);
+    insertionSort(ar);
+    displayIntArray(ar);
     //URL* u1 = new URL("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
     //cout << u1->getContents() << endl;
     //string jsonString = "[{\"fname\":\"val1\", \"lname\": \"val2\", \"sums\":[1,2,3]},{\"fname\":\"val3\", \"lname\":\"val4\"}]";
     //cout << jsonString << endl;
     //processJSONObject(jsonString, 0);
+    /*
     LinkedList* ll = new LinkedList();
     ll->addEnd(4);
     ll->addEnd(6);
@@ -27,8 +50,33 @@ int main(int argc, char** argv)
     cout << value << endl;
     ll->addAtIndex(6, 2);
     ll->display();
-    
+    */
     return 0;
+}
+
+void displayIntArray(int ar[])
+{
+    for(int i = 0; i < ARRAY_LENGTH; i++)
+    {
+        cout << ar[i] << endl;
+    }
+}
+
+void insertionSort(int ar[])
+{
+    int tempSwap;
+    int theFollower;
+    for(int currStart = 0; currStart < ARRAY_LENGTH; currStart++)
+    {
+        theFollower = currStart;
+        while(theFollower > 0 && ar[theFollower] < ar[theFollower-1])
+        {
+            tempSwap = ar[theFollower];
+            ar[theFollower] = ar[theFollower-1];
+            ar[theFollower-1] = tempSwap;
+            theFollower--;
+        }
+    }
 }
 
 string processJSONObject(string json, int start)
